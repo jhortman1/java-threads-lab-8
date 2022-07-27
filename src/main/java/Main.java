ed83dbf5b080a67655575c236efb08056376f63f
James Hortman <jamesdhortman@gmail.com>
2022-07-27 11:32:27 -0700

    Threads lab 8 complerte

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,11 @@ import java.util.concurrent.Future;
 public class Main {
     public static int countFinishedFutures(List<Future> futures) {
         // your code here
+        int total = 0;
+        for (Future f : futures)
+        {
+            if (f.isDone()) total++;
+        }
+        return total;
     }
 }
